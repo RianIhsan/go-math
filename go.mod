2b7262f231ab956859db9332ff6df44f6aeefac9
RianIhsan <Rianwhatsup@gmail.com>
2023-03-06 04:20:44 +0700

    New modul go-math

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RianIhsan/go-math
+
+go 1.20
---
+++ math_operation.go
@@ -0,0 +1,9 @@
+package go_math
+
+import "fmt"
+
+func MathOp(x, y int) {
+	var res int
+	res = x + y
+	fmt.Println("Hasil: ", res)
+}
