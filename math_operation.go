916fea9a37d73692adf0f1fdc4bf245da453b5d8
RianIhsan <Rianwhatsup@gmail.com>
2023-03-06 04:44:00 +0700

    Update Kalkulaotr with Modules

--- math_operation.go
+++ math_operation.go
@@ -2,8 +2,18 @@ package go_math
 
 import "fmt"
 
-func MathOp(x, y int) {
+func Pertambahan(x, y int) {
 	var res int
 	res = x + y
-	fmt.Println("Hasil: ", res)
+	fmt.Println("Masil Dari Pertambahan ", x, " + ", y, " = ", res)
+}
+func Pengurangan(x, y int) {
+	var res int
+	res = x - y
+	fmt.Println("Masil Dari Pengurangan ", x, " - ", y, " = ", res)
+}
+func Perkalian(x, y int) {
+	var res int
+	res = x - y
+	fmt.Println("Masil Dari Perkalian ", x, " * ", y, " = ", res)
 }
